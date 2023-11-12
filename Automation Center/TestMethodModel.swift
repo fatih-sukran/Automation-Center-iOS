2e5ffe51fd993e3dcf304b9ce00a87b79913a07a
Fatih Şükran <fth.21.81@gmail.com>
2023-11-12 10:21:32 +0300

    make testmethod decodable

--- Automation Center/TestMethodModel.swift
+++ Automation Center/TestMethodModel.swift
@@ -9,12 +9,26 @@ import Foundation
 import SwiftData
 
 @Model
-final class TestMethod {
-    var id: Int
-    var name: String
-    
+final class TestMethod: Decodable {
+    let id: Int
+    let name: String
+
     init(id: Int, name: String) {
         self.id = id
         self.name = name
     }
+
+    // Implement the required initializer for Decodable conformance.
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+    }
+
+    // If your CodingKeys are different from the property names, define them.
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+    }
 }
+
